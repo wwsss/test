bfe52f493a3d7a99e4da62b65b22c742a90bcfef
wwsss <15038556602@163.com>
2024-09-17 10:18:05 +0800

    1

---
+++ src/main/java/com/wollo/Main.java
@@ -0,0 +1,15 @@
+package com.wollo;
+
+/**
+ * @BelongsProject: Default (Template) Project
+ * @BelongsPackage: com.wollo
+ * @Author: 她说不坐迈腾380
+ * @CreateTime: 2024-09-17  09:26
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
